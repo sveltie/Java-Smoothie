58b4ee9b2f6a9443944ce03efefe0435e5bdceb4
sveltie <lucydabestt@gmail.com>
2023-04-05 16:56:28 +0700

    (Add): Some empty files for template

---
+++ data_structures/implementation/NetworkPacketProcessing.java
@@ -0,0 +1,5 @@
+package implementation;
+
+public class NetworkPacketProcessing {
+    // for now it's empty
+}
---
+++ data_structures/tree/BinaryTree.java
@@ -0,0 +1,5 @@
+package tree;
+
+public class BinaryTree {
+    // for now it's empty
+}
